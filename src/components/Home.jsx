c1999df3b3562ad734bbe4fcba88a5f4058a591a
Trairat Kaewsaad <121665180+eurekaMarch@users.noreply.github.com>
2024-02-01 11:05:27 +0700

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,68 +5,78 @@ import profile from "../assets/myProfile.png";
 
 function Home() {
   return (
-    <Box
-      id="Home"
-      sx={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        mx: "8rem",
-        mt: "5rem",
-        mb: "2rem",
-      }}
-    >
-      <Box>
-        <Typography
-          sx={{
-            fontSize: "12rem",
-            fontFamily: "Black Ops One",
-            lineHeight: "12rem",
-          }}
-        >
-          Trairat <br /> Kaewsaad
-        </Typography>
-
-        <Box sx={{ width: "81%" }}>
+    <Box>
+      <Box
+        id="Home"
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          mx: "8rem",
+          mt: "6rem",
+          mb: "8rem",
+        }}
+      >
+        <Box>
           <Typography
             sx={{
-              color: "#8C8E8E",
-              mt: "1rem",
-              fontSize: "2.5rem",
-              fontWeight: 600,
+              fontSize: "12rem",
+              fontFamily: "Black Ops One",
+              lineHeight: "12rem",
             }}
           >
-            Full-Stack Developer
+            Trairat <br /> Kaewsaad
           </Typography>
 
-          <Typography
-            sx={{
-              textAlign: "justify",
-              textIndent: 50,
-              color: "#8C8E8E",
-              fontSize: "1.4rem",
-            }}
-          >
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Praesentium adipisci sequi dolorum at voluptate eius? Asperiores ad
-            laudantium quae eius consectetur quaerat esse quia ipsam cupiditate
-            in repudiandae cumque excepturi delectus illum sit libero, culpa
-            reiciendis incidunt minima necessitatibus, voluptatem sint eos
-            perspiciatis! Inventore ab necessitatibus iste quis. Molestiae,
-            molestias!
-          </Typography>
+          <Box sx={{ width: "81%" }}>
+            <Typography
+              sx={{
+                color: "#8C8E8E",
+                mt: "1rem",
+                fontSize: "2.5rem",
+                fontWeight: 600,
+              }}
+            >
+              Full-Stack Developer
+            </Typography>
+
+            <Typography
+              sx={{
+                textAlign: "justify",
+                textIndent: 50,
+                color: "#8C8E8E",
+                fontSize: "1.4rem",
+              }}
+            >
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Praesentium adipisci sequi dolorum at voluptate eius? Asperiores
+              ad laudantium quae eius consectetur quaerat esse quia ipsam
+              cupiditate in repudiandae cumque excepturi delectus illum sit
+              libero, culpa reiciendis incidunt minima necessitatibus,
+              voluptatem sint eos perspiciatis! Inventore ab necessitatibus iste
+              quis. Molestiae, molestias!
+            </Typography>
+          </Box>
         </Box>
+
+        <CardMedia
+          component="img"
+          image={profile}
+          sx={{
+            width: "24rem",
+            height: "35rem",
+            borderRadius: "0.5rem",
+          }}
+        />
       </Box>
 
-      <CardMedia
-        component="img"
-        image={profile}
-        sx={{
-          width: "24rem",
-          height: "35rem",
-          borderRadius: "0.5rem",
-        }}
-      />
+      <Box sx={{ mb: "8rem" }}>
+        <Typography sx={{ ml: "3rem", fontSize: "3rem", fontWeight: "bold" }}>
+          SKILLS
+        </Typography>
+
+        <Box></Box>
+      </Box>
     </Box>
   );
 }
