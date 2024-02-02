21bfc9c92ebaf61425b5a9dc702a27aa5312566d
Trairat Kaewsaad <121665180+eurekaMarch@users.noreply.github.com>
2024-02-02 10:20:22 +0700

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Unstable_Grid2";
 import Button from "@mui/material/Button";
 
 function About() {
@@ -15,70 +16,105 @@ function About() {
       >
         ABOUT ME
       </Typography>
-      <Box sx={{ mx: "3rem", display: "flex", justifyContent: "space-around" }}>
-        <a href="">
-          <Button
-            variant="contained"
-            color="green"
-            sx={{
-              fontSize: "1.6rem",
-              height: "5rem",
-            }}
-          >
-            <Typography>
-              <i className="fa-regular fa-file"></i> resume
-            </Typography>
-          </Button>
-        </a>
-
-        <a href="mailto:trairatk1984@gmail.com">
-          <Button
-            variant="contained"
-            color="green"
-            sx={{
-              fontSize: "1.6rem",
-              height: "5rem",
-            }}
-          >
-            <Typography>
-              <i className="fa-regular fa-envelope"></i> email
-            </Typography>
-          </Button>
-        </a>
-
-        <a
-          href="https://www.linkedin.com/in/trairat-kaewsaad-5028732aa"
-          target="blank"
+      {/* <Box sx={{ mx: "3rem", display: "flex", justifyContent: "space-around" }}> */}
+      <Grid container xs={12} sm={12} md={12}>
+        <Grid
+          xs={3}
+          sm={3}
+          md={3}
+          sx={{ px: { xs: "0.5rem", sm: "2rem", md: "3rem" } }}
         >
-          <Button
-            variant="contained"
-            color="green"
-            sx={{
-              fontSize: "1.6rem",
-              height: "5rem",
-            }}
-          >
-            <Typography>
-              <i className="fa-brands fa-linkedin"></i> linkedin
-            </Typography>
-          </Button>
-        </a>
+          <a href="">
+            <Button
+              variant="contained"
+              color="green"
+              sx={{
+                fontSize: { xs: "1rem", sm: "1.4rem", md: "1.6rem" },
+                height: "4rem",
+                width: "100%",
+              }}
+            >
+              <Typography>
+                <i className="fa-regular fa-file"></i> resume
+              </Typography>
+            </Button>
+          </a>
+        </Grid>
 
-        <a href="https://github.com/eurekaMarch" target="blank">
-          <Button
-            variant="contained"
-            color="green"
-            sx={{
-              fontSize: "1.6rem",
-              height: "5rem",
-            }}
+        <Grid
+          xs={3}
+          sm={3}
+          md={3}
+          sx={{ px: { xs: "0.5rem", sm: "2rem", md: "3rem" } }}
+        >
+          <a href="mailto:trairatk1984@gmail.com">
+            <Button
+              variant="contained"
+              color="green"
+              sx={{
+                fontSize: { xs: "1rem", sm: "1.4rem", md: "1.6rem" },
+                height: "4rem",
+                width: "100%",
+              }}
+            >
+              <Typography>
+                <i className="fa-regular fa-envelope"></i> email
+              </Typography>
+            </Button>
+          </a>
+        </Grid>
+
+        <Grid
+          xs={3}
+          sm={3}
+          md={3}
+          sx={{ px: { xs: "0.5rem", sm: "2rem", md: "3rem" } }}
+        >
+          <a
+            href="https://www.linkedin.com/in/trairat-kaewsaad-5028732aa"
+            target="blank"
           >
-            <Typography>
-              <i className="fa-brands fa-github"></i> github
-            </Typography>
-          </Button>
-        </a>
-      </Box>
+            <Button
+              variant="contained"
+              color="green"
+              sx={{
+                fontSize: { xs: "1rem", sm: "1.4rem", md: "1.6rem" },
+                height: "4rem",
+                width: "100%",
+              }}
+            >
+              <Typography>
+                <i className="fa-brands fa-linkedin"></i> linkedin
+              </Typography>
+            </Button>
+          </a>
+        </Grid>
+
+        <Grid
+          xs={3}
+          sm={3}
+          md={3}
+          sx={{ px: { xs: "0.5rem", sm: "2rem", md: "3rem" } }}
+        >
+          <a href="https://github.com/eurekaMarch" target="blank">
+            <Button
+              variant="contained"
+              color="green"
+              sx={{
+                fontSize: { xs: "1rem", sm: "1.4rem", md: "1.6rem" },
+                height: "4rem",
+                width: "100%",
+              }}
+            >
+              <Typography>
+                <i className="fa-brands fa-github"></i> github
+              </Typography>
+            </Button>
+          </a>
+        </Grid>
+      </Grid>
+
+      {/* </Box> */}
     </Box>
   );
 }
